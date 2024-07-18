388fe9b165e534bfe6f9ffaddc680dfba01667a4
Xinxian <xinxian.wu@onelab.tw>
2024-07-18 21:09:00 +0800

    feat: support kitty

--- RunCat/Program.cs
+++ RunCat/Program.cs
@@ -93,6 +93,10 @@ namespace RunCat
                 new ToolStripMenuItem("Horse", null, SetRunner)
                 {
                     Checked = runner.Equals("horse")
+                },
+                new ToolStripMenuItem("Kitty", null, SetRunner)
+                {
+                    Checked = runner.Equals("kitty")
                 }
             });
 
@@ -220,6 +224,10 @@ namespace RunCat
             else if (runner.Equals("horse")) 
             {
                 capacity = 14;
+            }
+            else if (runner.Equals("kitty")) 
+            {
+                capacity = 20;
             }
             List<Icon> list = new List<Icon>(capacity);
             for (int i = 0; i < capacity; i++)
